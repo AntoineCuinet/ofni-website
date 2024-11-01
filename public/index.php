13858b797aa4da8c54e11da14c0d592bafef3452
AntoineCuinet <antoine.cuinetdu70200@gmail.com>
2024-11-01 18:51:42 +0100

    Add Symfony

--- public/index.php
+++ public/index.php
@@ -1,12 +1,9 @@
 <?php
-/* ========================================================================
- * File Name: index.php
- * Author: CUINET Antoine
- * Version: 1.0
- * Date: September 2024
- *
- * Note: This code was developed by CUINET Antoine, see https://acuinet.fr
-======================================================================== */
-require_once __DIR__ . '/assets/php/index.php';
-// Silence is golden.
-?>
+
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
